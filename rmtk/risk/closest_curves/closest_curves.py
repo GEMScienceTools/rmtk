afc38e2ef984c5a6c534da4cde90550e97749b30
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-06-17 17:06:51 +0200

    adaptive mdof->sdof script, notebook

--- rmtk/risk/closest_curves/closest_curves.py
+++ rmtk/risk/closest_curves/closest_curves.py
@@ -6,13 +6,80 @@ import operator
 import numpy as np
 import glob
 from lxml import etree
+from collections import OrderedDict
+from collections import namedtuple
 
-try:
-    from openquake.nrmllib.hazard.parsers import HazardCurveXMLParser
-except:
-    from openquake.nrmllib.hazard.parsers import HazardCurveParser
-    HazardCurveXMLParser = HazardCurveParser
+def iterparse_tree(source, events=('start', 'end')):
+    return etree.iterparse(source, events=events)
 
+class HazardCurveModel(object):
+    """
+    Simple container for hazard curve objects. The accepted arguments
+    are::
+
+        * investigation_time
+        * imt
+        * imls
+        * statistics
+        * quantile_value
+        * sa_period
+        * sa_damping
+        * data_iter (optional), an iterable returning pairs with the form
+          (poes_array, location).
+    """
+
+    def __init__(self, **metadata):
+        self._data_iter = metadata.pop('data_iter', ())
+        self.metadata = metadata
+        vars(self).update(metadata)
+
+    def __iter__(self):
+        return self._data_iter
+
+
+HazardCurveData = namedtuple('HazardCurveData', 'location poes')
+Location = namedtuple('Location', 'x y')
+
+
+class HazardCurveXMLParser(object):
+    _CURVES_TAG = '{%s}hazardCurves'
+    _CURVE_TAG = '{%s}hazardCurve'
+
+    def __init__(self, source):
+        self.source = source
+
+    def parse(self):
+        """
+        Parse the source XML content for a hazard curve.
+        :returns:
+            Populated :class:`HazardCurveModel` object
+        """
+        tree = iterparse_tree(self.source)
+        hc_iter = self._parse(tree)
+        header = hc_iter.next()
+        return HazardCurveModel(data_iter=hc_iter, **header)
+
+    def _parse(self, tree):
+        header = OrderedDict()
+        for event, element in tree:
+            if element.tag == self._CURVES_TAG and event == 'start':
+                a = element.attrib
+                header['statistics'] = a.get('statistics')
+                header['quantile_value'] = a.get('quantileValue')
+                header['smlt_path'] = a.get('sourceModelTreePath')
+                header['gsimlt_path'] = a.get('gsimTreePath')
+                header['imt'] = a['IMT']
+                header['investigation_time'] = a['investigationTime']
+                header['sa_period'] = a.get('saPeriod')
+                header['sa_damping'] = a.get('saDamping')
+                header['imls'] = map(float, element[0].text.split())
+                yield header
+            elif element.tag == self._CURVE_TAG and event == 'end':
+                point, poes = element
+                x, y = [float(v) for v in point[0].text.split()]
+                location = Location(x, y)
+                poes_array = map(float, poes.text.split())
+                yield HazardCurveData(location, poes_array)
 
 def rmse(predictions, targets):
     difference = abs(predictions - targets)
