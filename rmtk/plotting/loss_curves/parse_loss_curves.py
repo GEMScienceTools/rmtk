f9c6740cafc8a9d5537d5ee0b454023c6f4a804d
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-10-15 16:14:47 +0200

    Update notebooks to read NRML 0.5

--- rmtk/plotting/loss_curves/parse_loss_curves.py
+++ rmtk/plotting/loss_curves/parse_loss_curves.py
@@ -10,12 +10,12 @@ import numpy as np
 from lxml import etree
 from collections import OrderedDict
 
-xmlNRML='{http://openquake.org/xmlns/nrml/0.4}'
+xmlNRML='{http://openquake.org/xmlns/nrml/0.5}'
 xmlGML = '{http://www.opengis.net/gml}'
 
 def parse_single_loss_curve(element):
     '''
-    Reads the loss curve element to return the longitude, latitude and 
+    Reads the loss curve element to return the longitude, latitude and
     poes and losses
     '''
     for e in element.iter():
